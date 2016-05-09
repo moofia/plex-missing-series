057caccfda1b7e368f2f6155eebb64780308c44b
Jacques Atlas <atlas@wire.org.za>
2016-05-09 18:22:16 +0200

    quick change to output html as very basic datatable

--- lib/html.rb
+++ lib/html.rb
@@ -1,20 +1,85 @@
 # home of everything html output related
 
 def html_header
+  html = <<-HTML
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<title>Plex missing episodes</title>
+
+  	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
+  	<style type="text/css" class="init"></style>
+  	<script type="text/javascript" language="javascript" src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+  	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+  	
+    <script type="text/javascript" class="init">
+      $(document).ready(function() {
+  	    $('#example').DataTable( {
+  		    "order": [[ 3, "desc" ]]
+  	      } );
+       } );
+  	</script>
+       
+  </head>
+  <body>
+  HTML
+  puts html
 end
 
 def html_footer
+  html = <<-HTML 
+  </body>
+  </html>
+  HTML
+  puts html
 end
 
-def html_row(text, kat, bay)
+def html_table_start
   html = <<-HTML 
-  #{text} | <a target=\"blank\" href=\"#{kat}\">kat</a> | <a target=\"blank\" href=\"#{bay}\">bay</a> <br>
+				<table id="example" class="display" cellspacing="0" width="100%">
+					<thead>
+						<tr>
+							<th>Episode</th>
+							<th>Air Date</th>
+							<th></th>
+							<th></th>
+						</tr>
+					</thead>
+					<tfoot>
+						<tr>
+							<th>Episode</th>
+							<th>Air Date</th>
+							<th></th>
+							<th></th>
+						</tr>
+					</tfoot>
+					<tbody>
+  HTML
+  puts html
+end
+
+def html_table_end
+  html = <<-HTML 
+					</tbody>
+				</table>
+  HTML
+  puts html
+end
+
+def html_table_row(text, kat, bay)
+  html = <<-HTML 
+	<tr>
+		<td>#{text}</td>
+		<td>n/a</td>
+		<td><a target=\"blank\" href=\"#{kat}\">kat</a></td>
+		<td><a target=\"blank\" href=\"#{bay}\">bay</a></td>
+	</tr>
   HTML
   puts html
 end
 
 # takes a Hash of HASH[SHOW][SEASON][EPISODE]
-def html_content(episodes_missing)
+def html_table_content(episodes_missing)
   episodes_missing.keys.each do |show|
     episodes_missing[show].keys.each do |season|
       episodes_missing[show][season].keys.each do |episode|
@@ -22,7 +87,7 @@ def html_content(episodes_missing)
         data = URI.escape(show + ' ' + pair)
         kat  = "https://kat.cr/usearch/%22#{data}%20category%3Atv/?field=seeders&sorder=desc"
         bay  = "http://thepiratebay.se/search/#{data}/0/7/200"
-        html_row("#{show} #{pair}" , kat, bay)
+        html_table_row("#{show} #{pair}" , kat, bay)
       end
     end
   end
@@ -30,6 +95,8 @@ end
 
 def html_create(episodes_missing)
   html_header
-  html_content episodes_missing
+  html_table_start
+  html_table_content episodes_missing  
+  html_table_end
   html_footer
 end
