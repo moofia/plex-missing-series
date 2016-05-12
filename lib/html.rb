48890b57bb19d3b75f63326e6f935b6f75ba7a21
Jacques Atlas <atlas@wire.org.za>
2016-05-12 10:21:19 +0200

    cosmetic changes

--- lib/html.rb
+++ lib/html.rb
@@ -5,13 +5,20 @@ def html_header
   <!DOCTYPE html>
   <html>
   <head>
-  	<title>Plex missing episodes</title>
-
+  
+    <!-- DataTables -->
   	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
-  	<style type="text/css" class="init"></style>
   	<script type="text/javascript" language="javascript" src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
   	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
   	
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
     <script type="text/javascript" class="init">
       $(document).ready(function() {
   	    $('#episodes').DataTable( {
@@ -23,6 +30,12 @@ def html_header
        
   </head>
   <body>
+ 	<title>Plex missing episodes</title>
+
+  <h1>PLEX<small>missing episodes</small></h1>
+  <br>
+  <br>
+  
   HTML
   puts html
 end
