b0b2e8fd0c68fcc236a90129fe030c4ba754e4ae
Jacques Atlas <atlas@wire.org.za>
2016-05-14 09:36:35 +0200

    refactoring

--- lib/thetvdb_missing.rb
+++ lib/thetvdb_missing.rb
@@ -1,6 +1,7 @@
 # all work related to finding the last episode from thetvdb
 
-def thetvdb_missing_last_ep(episodes_plex,show)
+# plex last episode of a show
+def thetvdb_missing_plex_last_ep(episodes_plex,show)
   log_debug
   last_ep = '0;0'
   episodes_plex[show].keys.each do |season|
@@ -12,7 +13,8 @@ def thetvdb_missing_last_ep(episodes_plex,show)
   return last_ep.split(';')
 end
 
-def thetvdb_missing_first_ep(episodes_plex,show)
+# plex first episode of a show
+def thetvdb_missing_plex_first_ep(episodes_plex,show)
   log_debug
   first_ep = '0;0'
   episodes_plex[show].keys.each do |season|
@@ -26,45 +28,60 @@ def thetvdb_missing_first_ep(episodes_plex,show)
   return first_ep.split(';')
 end
 
+# remove episodes that are found in plex
+def thetvdb_missing_plex_found(episodes_plex, show,season,episode)
+  plex_has = false
+  
+  # remove shows that we have
+  if episodes_plex.has_key? show 
+    if episodes_plex[show].has_key?(season.to_i)
+      if episodes_plex[show][season.to_i].has_key?(episode.to_i)
+        plex_has = true              
+      end
+    end
+  end
+  plex_has
+end
+
+def thetvdb_missing_range(season, season_first, episode, episode_first)
+  missing = true
+  
+  # for now we are only interested in episodes greater than our first one and 
+  # inclusive of the whole season
+  if season.to_i < season_first.to_i 
+    missing = false
+  end
+  
+  if $config["missing"]["start_at_first_found"]
+    if episode_first.to_i > episode.to_i 
+      missing = false            
+    end
+  end
+  missing
+end
+
+# loop through all the episodes found in thetvdb for a show
+# remove those that exists in plex
+# ignore specials for now
 def thetvdb_missing_last_process(episodes_plex,episodes_missing,show)
   log_debug
   thetvdb_episodes            = thetvdb_find(show)
-  season_last, episode_last   = thetvdb_missing_last_ep(episodes_plex,show)
-  season_first, episode_first = thetvdb_missing_first_ep(episodes_plex,show)
+  season_last, episode_last   = thetvdb_missing_plex_last_ep(episodes_plex,show)
+  season_first, episode_first = thetvdb_missing_plex_first_ep(episodes_plex,show)
   
   thetvdb_episodes.keys.each do |show|
     thetvdb_episodes[show]['episodes'].keys.each do |season|
       next if season == "0"
       thetvdb_episodes[show]['episodes'][season].keys.each do |episode|
         first_aired = thetvdb_episodes[show]['episodes'][season][episode]['first_aired']
-        show_index = show_index(season, episode)
-        
-        plex_has = false
-        missing  = true
-        
-      
-        # remove shows that we have
-        if episodes_plex.has_key? show 
-          if episodes_plex[show].has_key?(season.to_i)
-            if episodes_plex[show][season.to_i].has_key?(episode.to_i)
-              plex_has = true              
-            end
-          end
-        end
-      
-        # for now we are only interested in episodes greater than our first one and 
-        # inclusive of the whole season
-        if season.to_i < season_first.to_i 
-          missing = false
-        end
-        
-        if $config["missing"]["start_at_first_found"]
-          if episode_first.to_i > episode.to_i 
-            missing = false            
-          end
-        end
-        
-        missing = false if plex_has
+        show_index  = show_index(season, episode)
+        plex_has    = false
+        missing     = true
+
+        plex_has    = thetvdb_missing_plex_found(episodes_plex,show,season,episode)
+        missing     = thetvdb_missing_range(season, season_first, episode, episode_first)
+
+        missing     = false if plex_has
                 
         if first_aired =~ /\w/
           date_available = Date.today
